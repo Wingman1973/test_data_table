e43f9e4260effc865a9e1c0618a010d1cefcbf92
Jose Luis Hidalgo <jlhidalgo@meteo.cat>
2019-01-17 13:01:16 +0100

    Fetching Data with HOC

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DataTable from "./components/DataTable";
 import Background from "./components/Background";
 import DataAsync from "./components/DataAsync";
 import ShowYouTube from "./components/ShowYouTube";
+import withFetching from "./components/DataWithFetching";
 import ErrorBoundary from "./components/ErrorBoundary";
 
 class App extends Component {
@@ -12,17 +13,20 @@ class App extends Component {
     (function() {
       console.log(this);
     })();
-
     // Catch errors in any components below and re-render with error message
     console.log("componentDidCatch App");
     // You can also log error messages to an error reporting service here
   }
 
   render() {
+    const API = "https://hn.algolia.com/api/v1/search?query=";
+    const DEFAULT_QUERY = "redux";
+    const DataWithHOC = withFetching(API + DEFAULT_QUERY)(DataAsync);
+
     return (
       <div className="App">
         <ErrorBoundary>
-          <DataAsync />
+          <DataWithHOC />
           <DataTable />
           <ShowYouTube />
           <Background />
